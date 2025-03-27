99c2d40d4810806faa94e7b724fef7b3e73a4f18
sauroh <rhopinaldo@gmail.com>
2025-03-27 14:38:51 +0800

    Added product detail screen

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Box } from "@/components/ui/box";
 import { Heading } from "@/components/ui/heading";
 import { Card } from "@/components/ui/card";
 import { Image } from "@/components/ui/image";
 import { Text } from "@/components/ui/text";
-import { VStack } from "@/components/ui/vstack";
-import { Button, ButtonText } from "@/components/ui/button";
+import { Link } from "expo-router";
+import { Pressable } from "react-native";
 
 interface Product {
     image: string | undefined;
@@ -17,7 +16,10 @@ interface Product {
   
   export default function ProductListItem({ product }: { product: Product }) {
     return (
-       // removed m-3 margin and set it index.ts flatlist using contentContainerClassName
+      
+       // removed m-3 margin from card and set it index.ts flatlist using contentContainerClassName
+      <Link href={`/product/${product.id}`} asChild>
+      <Pressable className="flex-1">
       <Card className="p-5 rounded-lg max-w-[360px] flex-1"> 
      
       <Image
@@ -31,27 +33,13 @@ interface Product {
       <Text className="text-sm font-normal mb-2 text-typography-700">
         {product.name}
       </Text>
-      <VStack className="mb-6">
+      
         <Heading size="md" className="mb-4">
           ${product.price}
         </Heading>
-        <Text size="sm">
-          {product.description}
-        </Text>
-      </VStack>
-      <Box className="flex-col sm:flex-row">
-        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-          <ButtonText size="sm">Add to cart</ButtonText>
-        </Button>
-        <Button
-          variant="outline"
-          className="px-4 py-2 border-outline-300 sm:flex-1"
-        >
-          <ButtonText size="sm" className="text-typography-600">
-            Wishlist
-          </ButtonText>
-        </Button>
-      </Box>
+
     </Card>
+    </Pressable>
+    </Link>
     );
   }
